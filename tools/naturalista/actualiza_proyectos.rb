a412c4d941bb7352c4cda00c00513f72d7d3654d
Carlos Alonso <calonsogeek@gmail.com>
2021-07-12 17:46:15 -0500

    linea para hacer pruebas

--- tools/naturalista/actualiza_proyectos.rb
+++ tools/naturalista/actualiza_proyectos.rb
@@ -19,6 +19,7 @@ start_time = Time.now
 
 if ARGV.count == 2
   paginas = (ARGV[0].to_i..ARGV[1].to_i).to_a
+  #Rails.logger.debug paginas.inspect
   Naturalista::Estadistica.actualizaProyectos(paginas: paginas)
 else
   Rails.logger.debug "Los argumentos necesitan ser dos: pagina inicial y pagina final" if OPTS[:debug]
