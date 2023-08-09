06d2e15a6b57e0a4ebc8094dde03fdbe8ec881c3
Alberto González <albertoglezba@gmail.com>
2023-08-09 09:28:17 -0600

    sugerencias de LRojas

--- app/helpers/eventos_helper.rb
+++ app/helpers/eventos_helper.rb
@@ -6,11 +6,11 @@ module EventosHelper
   def dameFormato(formato)
     case formato
     when Calendario::Evento::FORMATO[0]
-      "<i class='fa fa-street-view'></i> #{formato}".html_safe
+      "<i class='fa fa-street-view'></i> #{formato} <br>".html_safe
     when Calendario::Evento::FORMATO[1]
-      "<i class='fa fa-wifi'></i> #{formato}".html_safe
+      "<i class='fa fa-wifi'></i> #{formato} <br>".html_safe
     when Calendario::Evento::FORMATO[2]
-      "<i class='fa fa-street-view'>/</i><i class='fa fa-wifi'></i> #{formato}".html_safe
+      "<i class='fa fa-street-view'></i> / <i class='fa fa-wifi'></i> #{formato} <br>".html_safe
     else
       formato
     end
@@ -19,14 +19,16 @@ module EventosHelper
   def damePublicoMeta(publico)
     case publico
     when "Público General"
-    "<i class='fa fa-users'></i> #{publico}".html_safe
+    "<i class='fa fa-users'></i> #{publico} <br>".html_safe
     when "Educación Superior"
-    "<i class='fa fa-university'></i> #{publico}".html_safe
+    "<i class='fa fa-university'></i> #{publico} <br>".html_safe
     when /\AEducación/
-      "<i class='fa fa-book'></i> #{publico}".html_safe
+      "<i class='fa fa-book'></i> #{publico} <br>".html_safe
     else
       publico
     end
   end
+
+
 
 end
--- app/models/calendario/evento.rb
+++ app/models/calendario/evento.rb
@@ -1,7 +1,7 @@
 class Calendario::Evento < ApplicationRecord
     
     ACTIVIDAD = ["Conferencia / Plática", "Concierto", "Conversatorio", "Curso / Taller", "Difusión en medios de comunicación", "Entrevista", "Exposición fotográfica", "Foro", "Limpieza", "Observación de aves", "Presentación de libro", "Puertas abiertas", "Proyección", "Recorridos", "Reforestación", "Restauración", "Reunión", "Visita guiada", "Otra"].freeze
-    FORMATO = ["Híbrido", "Presencial", "Virtual"].freeze
+    FORMATO = ["Presencial", "Virtual", "Híbrido"].freeze
     PUBLICO_META = ["Público General", "Educación Básica", "Educación Media Superior", "Educación Superior", "Educación Continua", "Educación Especial", "Escuela Normal"].freeze
     ESTADO = ["A nivel nacional", "Aguascalientes", "Baja California", "Baja California Sur", "Campeche", "Chiapas", "Chihuahua", "Coahuila", "Colima", "Ciudad de México", "Durango", "Estado de México", "Guanajuato", "Guerrero", "Hidalgo", "Jalisco", "Michoacán", "Morelos", "Nayarit", "Nuevo León", "Oaxaca", "Puebla", "Querétaro", "Quintana Roo", "San Luis Potosí", "Sinaloa", "Tabasco", "Tamaulipas", "Tlaxcala", "Veracruz", "Yucatán", "Zacatecas"].freeze
 
