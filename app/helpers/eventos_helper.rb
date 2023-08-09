3860679ef6ec0b984cf869e84d68cfff5d0a170d
Alberto González <albertoglezba@gmail.com>
2023-08-09 08:22:11 -0600

    hay q arreglar esos switch, hacerlos hashes

--- app/helpers/eventos_helper.rb
+++ app/helpers/eventos_helper.rb
@@ -2,4 +2,31 @@ module EventosHelper
   def at_servicios(url)
     "/servicios#{url}"
   end
+
+  def dameFormato(formato)
+    case formato
+    when Calendario::Evento::FORMATO[0]
+      "<i class='fa fa-street-view'></i> #{formato}".html_safe
+    when Calendario::Evento::FORMATO[1]
+      "<i class='fa fa-wifi'></i> #{formato}".html_safe
+    when Calendario::Evento::FORMATO[2]
+      "<i class='fa fa-street-view'>/</i><i class='fa fa-wifi'></i> #{formato}".html_safe
+    else
+      formato
+    end
+  end
+
+  def damePublicoMeta(publico)
+    case publico
+    when "Público General"
+    "<i class='fa fa-users'></i> #{publico}".html_safe
+    when "Educación Superior"
+    "<i class='fa fa-university'></i> #{publico}".html_safe
+    when /\AEducación/
+      "<i class='fa fa-book'></i> #{publico}".html_safe
+    else
+      publico
+    end
+  end
+
 end
